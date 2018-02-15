ebce5f3d284dea626cf2410e3d039a8b13e71397
Michał Kolano <mkolano@sysinfo.com.pl>
2018-02-15 09:53:54 +0100

    pout renamed to poutbuffer

--- cpp/macro.c
+++ cpp/macro.c
@@ -472,10 +472,10 @@ builtin(Tokenrow *trp, int biname)
 	/* most are strings */
 	tp->type = STRING;
 	if (tp->wslen) {
-		*outp++ = ' ';
+		*outbufp++ = ' ';
 		tp->wslen = 1;
 	}
-	op = outp;
+	op = outbufp;
 	*op++ = '"';
 	switch (biname) {
 
@@ -510,7 +510,7 @@ builtin(Tokenrow *trp, int biname)
 	}
 	if (tp->type==STRING)
 		*op++ = '"';
-	tp->t = (uchar*)outp;
-	tp->len = op - outp;
-	outp = op;
+	tp->t = (uchar*)outbufp;
+	tp->len = op - outbufp;
+	outbufp = op;
 }
